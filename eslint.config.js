557306f8ba22f4a41f79e227e62cc2daefeeee63
Govinda <govinda777@gmail.com>
2024-08-06 21:04:15 -0300

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,24 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-
-
-export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-];
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  rules: {
+    // Ajuste as regras conforme necessário
+    'react/react-in-jsx-scope': 'off', // Exemplo: Desativa a regra de React em escopo
+    '@typescript-eslint/no-unused-vars': 'warn', // Exemplo: Transforma erro em aviso
+    '@typescript-eslint/no-explicit-any': 'off', // Exemplo: Desativa a regra de uso de 'any'
+  },
+};
