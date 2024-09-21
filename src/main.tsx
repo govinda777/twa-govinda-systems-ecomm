28d8e11cf28677cc7cc8692d255562ed1ecb9e2f
Govinda <govinda777@gmail.com>
2024-09-21 10:49:59 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,8 @@ import { TonConnectUIProvider } from "@tonconnect/ui-react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-// this manifest is used temporarily for development purposes
-const manifestUrl =
-  "https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json";
+
+const manifestUrl = `${window.location.origin}/tonconnect-manifest.json`;
 
 const queryClient = new QueryClient({
   defaultOptions: { queries: { refetchOnWindowFocus: false } },
